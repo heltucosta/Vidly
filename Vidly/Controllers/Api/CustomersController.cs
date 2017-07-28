7823a8f9154c83e1756a5897e801320f69f7944a
heltucosta <heltucosta@gmail.com>
2017-07-28 14:44:45 -0300

    Criado endpoints para Web API e gerado DTOs(Data Transfer Objects) para garantir seguranca dos domain models

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using AutoMapper;
+using Vidly.DTOs;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/customers
+        [Route("api/customers")]
+        [HttpGet]
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+        }
+
+        //GET /api/customers/1
+        [Route("api/customers/{id}")]
+        [HttpGet]
+        public CustomerDto GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return Mapper.Map<Customer, CustomerDto>(customer);
+        }
+
+        //POST /api/customers
+        [Route("api/customers")]
+        [HttpPost]
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+
+            return customerDto;
+        }
+
+        //PUT /api/customers/1
+        [Route("api/customers/{id}")]
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            //Mapper.Map<CustomerDto, Customer>(customerDto, customerInDb);
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        //DELETE /api/customers/1
+        [Route("api/customers/{id}")]
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public int MembershipTypeId { get; set; } // recognizes as foreign key but do not load the object completely        
+
+        [Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
